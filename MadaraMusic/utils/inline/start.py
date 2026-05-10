343a3dc5c0f30f3e4284aa140cb6705b60527a68
ragini19854-prog <ragini.19854@gmail.com>
2026-05-10 08:33:27 +0530

    fancy font buttons: MadaraMusic/utils/inline/start.py

--- MadaraMusic/utils/inline/start.py
+++ MadaraMusic/utils/inline/start.py
@@ -27,11 +27,11 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=enums.ButtonStyle.PRIMARY),
-            InlineKeyboardButton(text="ᴄʟᴏɴᴇ", callback_data="clone_page", style=enums.ButtonStyle.DANGER),
+            InlineKeyboardButton(text="cℓσηє", callback_data="clone_page", style=enums.ButtonStyle.PRIMARY),
         ],
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", callback_data="support_page", style=enums.ButtonStyle.PRIMARY),
-            InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", callback_data="gib_source", style=enums.ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="ѕυρρσят", callback_data="support_page", style=enums.ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="ѕσυяcє", callback_data="gib_source", style=enums.ButtonStyle.PRIMARY),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper", style=enums.ButtonStyle.PRIMARY),
